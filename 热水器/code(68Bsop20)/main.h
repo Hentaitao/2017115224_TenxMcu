0dd2f1ea609e2290d793665864675965bc2b8457
Hentai <15976320192@139.com>
2020-01-01 22:14:19 +0800

    68Bsop24，目前只实现了点灯，按键数码管还不可用或不稳定，待研究

---
+++ 热水器/code(68Bsop20)/main.h
@@ -0,0 +1,25 @@
+#ifndef __main_h
+#define __main_h
+// Hal: exp: #define P_led P10 -----------------
+
+// Const: exp: #define D_data 1 ----------------
+
+// Globle Var -----------------------------------------
+#ifdef __main_c
+uint8_t modeValue; //pin脚模式标志
+#else
+extern uint8_t modeValue;
+
+#endif
+
+
+// Action Macro: exp: #define F_getData() ------
+#define Mode_Neg() modeValue = ~modeValue		//模式标志取反
+// Function ------------------------------------
+void delayMs(uint16_t time);
+void TimeProcess();
+void TaskSetting();
+void TaskProcess();
+void DisplayProcess();
+void led_disp();
+#endif
